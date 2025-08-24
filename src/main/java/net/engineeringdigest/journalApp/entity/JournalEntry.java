1f1182e8575df020bdc53e59bd9b2d23d46c141b
Ashok <ashokakumbar06@gmail.com>
2025-08-24 08:50:11 +0530

    D

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,13 +1,14 @@
 package net.engineeringdigest.journalApp.entity;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import net.engineeringdigest.journalApp.enums.Sentiment;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Document(collection = "journal_entries")
 @Data
